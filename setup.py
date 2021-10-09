0244b5554daf99a3ab4cac0a38da05be270ea512
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-10-09 07:40:48 -0700

    cleaning

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name = "spaghetti",
       author = "Harrison LaBollita",
       author_email = "hlabolli@asu.edu",
       description = "a small plotting program written in python for plotting band structures calculated from wien2k and VASP",
-      packages = ["spaghetti", "spaghetti.core"],
+      packages = ["spaghetti", "spaghetti.src"],
       install_requires =["numpy", "matplotlib", "numba"],
-      scripts = ["scripts/spaghetti"])
+      scripts = ["spaghetti/src/spaghetti"])
 
