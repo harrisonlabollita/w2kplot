7bce7f598395572e8ee9d773077926f2a0665808
harrisonlabollita <hlabollita0219@lions.piedmont.edu>
2021-10-14 22:34:46 -0700

    simple fixes

--- spaghetti/src/bands.py
+++ spaghetti/src/bands.py
@@ -28,6 +28,7 @@ class bands:
             sys.exit(1)
         self.args=args
         self.files()            # grab the necessary input files
+        
         if self.args.switch=="bands":
             self.plot_bands()             # plot
         elif self.args.switch=="fatbands":
@@ -49,6 +50,8 @@ class bands:
 
 
     def files(self):
+        #TODO: rewrite this to find all of the files that match the extension
+        # in the directory
         """load all the necessary files into the spaghetti class."""
         self.bands = glob.glob("*.spaghetti_ene")[0]
         self.klist   = glob.glob("*.klist_band")[0]
@@ -89,6 +92,7 @@ class bands:
                 self.klabel.append(self.arg2latex(line.strip().split()[0]))
                 self.high_symm.append(il)
         self.high_symm = [self.kpts[ind] for ind in self.high_symm]
+
     #def kpath(self):
     #    """get the high-symmetry points and labels"""
     #    # TODO Is there a better way to do this?
@@ -111,23 +115,21 @@ class bands:
         """get the Fermi energy (eF) from the case.scf file."""
         self.eF = float([line for line in open(self.scf).readlines() if ":FER" in line][-1].split()[-1].strip())
 
-
     def plot_bands(self):
         """program to create band structure plot"""
         self.band_data()
         self.kpath()
         plt.figure()
-        for b in range(len(self.Ek)):
-            plt.plot(self.kpts, self.Ek[b,:], "k-", lw=1.5)
+        for b in range(len(self.Ek)): plt.plot(self.kpts, self.Ek[b,:], "k-", lw=1.5)
         plt.xticks(self.high_symm, self.klabel)
         for k in self.high_symm: plt.axvline(k, color="k", lw=0.5)
         plt.axhline(0.0, color="k", lw=0.5)
         plt.ylabel(r"$\varepsilon - \varepsilon_{\mathrm{F}}$ (eV)")
-        plt.ylim(self.args.ymin, self.args.ymax);
+        plt.ylim(self.args.ymin, self.args.ymax); 
         plt.xlim(np.min(self.high_symm), np.max(self.high_symm));
 
         if self.args.save:
-            plt.savefig(self.args.save+".pdf", format="pdf")
+            plt.savefig(self.args.save+".pdf", format="pdf", dpi=300)
         else:
             plt.show()
 
@@ -137,10 +139,10 @@ class bands:
         control={}
         control.update(load)
         self.keywords={"klabels": None, 
-                   "colors": None, 
-                   "atoms" : None, 
-                   "orbitals": None,
-                   "weight": None}
+                       "colors": None, 
+                       "atoms" : None, 
+                       "orbitals": None,
+                       "weight": None}
         for key in self.keywords.keys():
             if key in control.keys():
                 self.keywords[key] = control[key]
@@ -188,12 +190,13 @@ class bands:
         default_cols = [["dodgerblue", "lightcoral", "gold", "forestgreen", "magenta"],
                         ["b", "r", "g", "y", "c"],
                         ["royalblue", "salmon", "lawngreen", "orange", "deeppink"]]
-        self.band_data()
-        self.fermi()
-        self.kpath()
+        self.band_data()       # get the bands
+        self.fermi()           # get εF
+        self.kpath()           # get the kpath
         fig, ax = plt.subplots()
+
         for b in range(len(self.Ek)): ax.plot(self.kpts, self.Ek[b,:], "k-", lw=1.5)
-        
+        # perform some checks to make sure we have necessary files  
         if self.qtl is None or self.struct is None or self.scf is None:    # checks for necessary files
             print("Spaghetti needs: case.qtl, case.struct, and case.scf to run fatbands!")
             sys.exit(1)
@@ -202,9 +205,10 @@ class bands:
             print(msg)
             sys.exit(1)
 
-        structure=struct()
-        self.load_init()
-
+        structure=struct() # initialize a struct object
+        
+        self.load_init() # load info from the spaghetti.init file
+        
         self.weight = self.args.weight if self.keywords["weight"] is None else self.keywords["weight"]
         
         if self.keywords["colors"] is None:
@@ -214,8 +218,6 @@ class bands:
         else:
             self.colors = self.keywords["colors"]
        
-
-        self.colors = default_cols if self.keywords["colors"] is None else self.keywords["colors"]
         for (a, at) in enumerate(self.keywords["atoms"]):
             for o in range(len(self.keywords["orbitals"][a])):
                 qtl           = open(self.qtl).readlines() 
@@ -223,8 +225,8 @@ class bands:
                 qtl           = qtl[start:]
                 E         = []
                 character = []
-                #TODO: speed this up somehow
-                for q, line in enumerate(qtl):
+                
+                for line in qtl:
                     if 'BAND' not in line:
                         if line.split()[1] == str(at):
                             E.append((float(line.split()[0]) - self.eF)*ry2eV)                      # wien2k interal units are Ry switch to eV
@@ -255,15 +257,15 @@ class bands:
             legend_handles=self.create_legend(structure)
             fig.legend(handles=legend_handles, loc="upper left", fontsize = 10)
 
-
         if self.keywords["klabels"] is None: 
             ax.set_xticks(self.high_symm)
             ax.set_xticklabels(self.klabel)
         else:
+            assert len(self.high_symm) == len(self.keywords["klabels"]), "number of k labels provided does not match number of high-symmetry points!"
             ax.set_xticks(self.high_symm)
             ax.set_xticklabels(self.keywords["klabels"])
 
         if self.args.save:
-            plt.savefig(self.args.save+".pdf", format="pdf")
+            plt.savefig(self.args.save+".png", format="png", dpi=300)
         else:
             plt.show()
